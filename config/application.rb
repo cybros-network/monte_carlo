160c63d5df1ebdb5659438dd6f67a83f84e67596
jasl <jasl9187@hotmail.com>
2023-07-22 01:47:43 +0800

    first commit

---
+++ config/application.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require_relative "boot"
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
+require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module MonteCarlo
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.1
+
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework nil
+    end
+
+    # overrides = "#{Rails.root}/app/overrides"
+    # Rails.autoloaders.main.ignore(overrides)
+    # config.to_prepare do
+    #   Dir.glob("#{overrides}/**/*_override.rb").each do |override|
+    #     load override
+    #   end
+    # end
+
+    # Read ActionMailer config from config/mailer.yml
+    initializer "action_mailer.set_configs.set_yaml_configs", before: "action_mailer.set_configs" do |app|
+      next unless File.exist?(Rails.root.join("config", "mailer.yml"))
+
+      configure = app.config_for("mailer").deep_symbolize_keys
+      configure.each do |key, value|
+        setter = "#{key}="
+        unless app.config.action_mailer.respond_to? setter
+          raise "Can't set option `#{key}` to ActionMailer, make sure that options in config/mailer.yml are valid."
+        end
+
+        app.config.action_mailer.send(setter, value)
+      end
+    end
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
