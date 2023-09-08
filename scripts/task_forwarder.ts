260bb4d735426ab559cbcdc660409d36b2ff9b11
jasl <jasl9187@hotmail.com>
2023-09-08 21:00:01 +0800

    bug fix

--- scripts/task_forwarder.ts
+++ scripts/task_forwarder.ts
@@ -182,7 +182,7 @@ router.post("/submit", async (ctx) => {
 
   console.info(`Sending offchainComputingPool.createJob(poolId, policyId, uniqueTrackId, implSpecVersion, input, softExpiresIn)`);
   const txPromise = api.tx.offchainComputingPool.createJob(
-    jobPoolId, jobPolicyId, uniqueTrackId, jobSpecVersion, JSON.stringify(input), null
+    jobPoolId, jobPolicyId, uniqueTrackId, jobSpecVersion, input, null
   );
 
   const callHash = txPromise.toHex();
